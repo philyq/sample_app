c36ff81bf8274abe56bf5f4be50adb27e4c4a55c
Philip Qumsieh <phily@shaw.ca>
2010-01-07 19:57:11 -0800

    Finished Chapter 2 Excercises. Closes gh-3

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,16 +3,22 @@ require 'spec_helper'
 describe PagesController do
   integrate_views
 
+  before(:each) do
+    #
+    # Define @base_title here.
+    #
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-
+    
     it "should have the right title" do
       get 'home'
-      response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_tag("title", @base_title + " | Home")
     end
   end
 
@@ -24,8 +30,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-      response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_tag("title", @base_title + " | Contact")
     end
   end
 
@@ -37,8 +42,8 @@ describe PagesController do
 
     it "should have the right title" do
       get 'about'
-      response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | About")
+      response.should have_tag("title", @base_title + " | About")
     end
   end
 end
+
